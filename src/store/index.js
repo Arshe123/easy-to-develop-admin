6da7fe2ea0d73d9b00df4f768f6cbff37b9c0af8
Arshe <2220318422@qq.com>
2026-05-16 18:26:03 +0800

    feat(store): 支持临时非持久化状态
    修改 set 和 remove 方法，新增 temp 参数以标记临时状态，临时状态不会被持久化到 localStorage，同时维护临时状态列表进行同步管理。

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,34 @@
 class GlobalStore {
   data = null;
+  tempData = null;
   GLOBAL_STORE_KEY = 'global-store-key';
   constructor() {
     this.data = JSON.parse(localStorage.getItem(this.GLOBAL_STORE_KEY) || '{}');
+    this.tempData = []
   }
   set(key, value, temp = false) {
     this.data[key] = value;
-    if (!temp) {
-      localStorage.setItem(this.GLOBAL_STORE_KEY, JSON.stringify(this.data));
+    if (temp) {
+      this.tempData.push(key);
     }
+    let newData = {...this.data};
+    this.tempData.forEach((item) => {
+      delete newData[item];
+    });
+    localStorage.setItem(this.GLOBAL_STORE_KEY, JSON.stringify(newData));
+
   }
   get(key) {
     return this.data[key] ?? null;
   }
   remove(key) {
     delete this.data[key];
-    localStorage.setItem(this.GLOBAL_STORE_KEY, JSON.stringify(this.data));
+    this.tempData = this.tempData.filter((item) => item !== key);
+    let newData = {...this.data};
+    this.tempData.forEach((item) => {
+      delete newData[item];
+    });
+    localStorage.setItem(this.GLOBAL_STORE_KEY, JSON.stringify(newData));
   }
 }
 
